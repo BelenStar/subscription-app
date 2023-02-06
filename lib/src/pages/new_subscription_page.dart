57976d57f91911ddb41178c7068b47c98915b401
Nellyannis Ramirez <nfrv04@gmail.com>
2023-02-06 02:09:05 -0400

    design form new subscription

--- lib/src/pages/new_subscription_page.dart
+++ lib/src/pages/new_subscription_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:suscription_payment/src/classes/subscription_item.dart';
+import 'package:suscription_payment/src/lists/subs_types_list.dart';
 
 class NewSubscriptionPage extends StatefulWidget {
+  const NewSubscriptionPage({super.key});
+
   @override
   createState() => _NewSubscriptionPageState();
 }
@@ -22,12 +25,48 @@ class _NewSubscriptionPageState extends State<NewSubscriptionPage> {
       ),
       body: Center(
         child: Form(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
             children: [
-              TextFormField(
+              TextField(
                 controller: name,
-              )
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    labelText: 'Service',
+                    hintText: 'spotify, netflix...',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black38),
+                      borderRadius: BorderRadius.all(Radius.circular(15.5)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.all(Radius.circular(15.5)),
+                    )),
+              ),
+              const SizedBox(height: 4),
+              DropdownButton(
+                  items: dropdownServicesTypes
+                      .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                      .toList(),
+                  value: dropdownServicesTypes,
+                  onChanged: (val) {}),
+              TextField(
+                controller: type,
+                //keyboardType: Tex,
+                decoration: const InputDecoration(
+                  labelText: 'Category',
+                  hintText: 'music, streaming...',
+                ),
+              ),
+              const SizedBox(height: 4),
+              TextField(
+                controller: price,
+                decoration: const InputDecoration(
+                  labelText: 'Price',
+                  hintText: 'monthly payment',
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Add'))
             ],
           ),
         ),
